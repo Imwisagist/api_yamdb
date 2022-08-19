ebde3da6cedc8e6703cd42d62d1b5b342ae63b3d
VladimirSolomakha <103119933+VladimirSolomakha@users.noreply.github.com>
2022-08-19 22:26:11 +0300

    Vladimir (#15)
    * First new Vova
    
    * Second review
    
    Co-authored-by: Спиряев Дмитрий Сергеевич <103213474+Imwisagist@users.noreply.github.com>

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -97,6 +97,17 @@ class GenreSerializer(serializers.ModelSerializer):
         exclude = ('id',)
 
 
+class TitleSerializerGet(serializers.ModelSerializer):
+    category = CategorySerializer()
+    genre = GenreSerializer(many=True)
+    rating = serializers.IntegerField(default=0)
+
+    class Meta:
+        model = Title
+        fields = '__all__'
+        read_only_fields = ('__all__',)
+
+
 class TitleSerializerPost(serializers.ModelSerializer):
     category = SlugRelatedField(
         slug_field='slug',
@@ -111,17 +122,9 @@ class TitleSerializerPost(serializers.ModelSerializer):
         validators=[MaxValueValidator(timezone.now().year)],
     )
 
-    class Meta:
-        model = Title
-        fields = '__all__'
-
-
-class TitleSerializerGet(serializers.ModelSerializer):
-    category = CategorySerializer()
-    genre = GenreSerializer(many=True)
-    rating = serializers.IntegerField()
+    def to_representation(self, value):
+        return TitleSerializerGet(self.instance).data
 
     class Meta:
         model = Title
         fields = '__all__'
-        read_only_fields = ('__all__',)
--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -95,7 +95,7 @@ class NameSlugModel(models.Model):
 
     class Meta:
         abstract = True
-        ordering = ('name', )
+        ordering = ('name',)
 
     def __str__(self):
         return f'{self.name}'
@@ -137,6 +137,9 @@ class Title(models.Model):
         verbose_name='Жанр',
     )
     description = models.TextField(verbose_name='Описание')
+
+    class Meta(NameSlugModel.Meta):
+        verbose_name = 'Произведения'
 
     def __str__(self):
         return f'{self.name}'
