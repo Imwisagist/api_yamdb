b06cd19b39353093833053c60adc2c80336bc328
Дмитрий Спиряев <imwisagist@yandex.ru>
2022-08-20 10:06:51 +0300

    Поправил пару импортов

--- api_yamdb/api/service.py
+++ api_yamdb/api/service.py
@@ -1,7 +1,6 @@
 from django_filters import rest_framework
 from rest_framework import filters, mixins, viewsets
 
-from .permissions import IsAdminOrReadOnly
 from reviews.models import Title
 from .permissions import IsAdminOrReadOnly
 
